6414f10eb9c7cd48c7104c2a68315af3e269ddb3
anantariskys <gamersindo555@gmail.com>
2026-01-05 19:58:29 +0700

    refactor(chat): replace custom layout with shared ChatLayout component

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -1,12 +1,5 @@
+import ChatLayout from "@/components/layout/chatLayout";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex h-screen w-full overflow-hidden bg-background">
-      {/* <Sidebar
-        chats={chats}
-        activeChatId={activeChatId}
-        onChatSelect={setActiveChatId}
-      /> */}
-    {children}
-    </div>
-  );
+  return <ChatLayout>{children}</ChatLayout>;
 }
